0b337f59860d9dc8c3a167423fa47924de14fccf
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-02-15 22:17:09 +0530

    Delete aws-scps-with-terraform-latest directory

--- aws-scps-with-terraform-latest/backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#    backend "s3" {
-#       # Replace this with your bucket name!
-#       bucket         = "terraform-state-lennar-infra"
-#       key            = "infra_services.tfstate"
-#       region         = "us-east-1"
-#   }
-# }
